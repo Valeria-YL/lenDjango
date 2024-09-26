ab0fdcf18bd4c1c52acec3bf959c80bf5898b75d
Valeria-YL <valeria.ybarral@uanl.edu.mx>
2024-09-25 23:11:38 -0600

    DJANGO STEP 2

--- lenDjango/views.py
+++ lenDjango/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
-def home(request):
-    return HttpResponse("Hello, Django!")
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
