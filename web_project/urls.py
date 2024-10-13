4cbcb7d3fbb2f398a3b0b0363bb0fee329d53aa9
Valeria-YL <valeria.ybarral@uanl.edu.mx>
2024-10-13 12:42:37 -0600

    DJANGO STEP 4 -UPDATE

--- web_project/urls.py
+++ web_project/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from lenDjango import views
 
 urlpatterns = [
-    path("", include("lenDjango.urls")),
-    path('admin/', admin.site.urls),
+    path("lenDjango/", include("lenDjango.urls")),
+    path("admin/", admin.site.urls),
 ]
